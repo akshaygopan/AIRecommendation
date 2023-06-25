96988717e25c73cbd4bf33f5091c056ae3a6c6a6
NarmadaH <achini.nh@gmail.com>
2023-06-24 22:18:18 -0400

    New apex methods

--- force-app/main/default/lwc/alPlanProcessNew/alPlanProcessNew.js
+++ force-app/main/default/lwc/alPlanProcessNew/alPlanProcessNew.js
@@ -10,6 +10,12 @@ import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 
 import getPlanRelatedLists from '@salesforce/apex/PlanRelatedListHandler.getPlanRelatedLists';
 import getStatementList from '@salesforce/apex/PlanRelatedListHandler.getStatementList';
+import getTest from '@salesforce/apex/PlanRelatedListHandler.getTest';
+import setActionIsSelected from '@salesforce/apex/PlanRelatedListHandler.setActionIsSelected';
+import getOtherActionList from '@salesforce/apex/PlanRelatedListHandler.getOtherActionList';
+import getOtherActionListbyCatergoty from '@salesforce/apex/PlanRelatedListHandler.getOtherActionListbyCatergoty';
+import setOtherAction from '@salesforce/apex/PlanRelatedListHandler.setOtherAction';
+import getPlanRelatedSummaryLists from '@salesforce/apex/PlanRelatedListHandler.getPlanRelatedSummaryLists';
 
 import SETTLEMENT_PLAN_OBJECT from '@salesforce/schema/Auto_Settlement_Plan__c';
 import SETTLEMENT_PLAN_FEILD_ID from '@salesforce/schema/Auto_Settlement_Plan__c.Settlement_Plan_ID__c';
@@ -26,6 +32,8 @@ import ACTION_FEILD_RECORD_ID from '@salesforce/schema/Recommended_Action__c.Id'
 
 
 export default class AlPlanProcessNew extends LightningElement {
+
+    
 
     @api recordId;
 
@@ -53,45 +61,133 @@ export default class AlPlanProcessNew extends LightningElement {
   
     @track recommendations;
     @track errorx;
-    @wire(getPlanRelatedLists, { planId: '$recordId' })
+    @wire(getTest, { planId: '$recordId' })
     wiredResult1({ error, data }) {
         console.log('wiredResult1');
         if (data) {
-            console.log('HAS DATA getPlanRelatedLists 1');
+            console.log('HAS DATA getTest 1');
          
             this.recommendations = data;
             console.log(this.recommendations);
 
         } else if (error) {
-            console.log('ERROR getPlanRelatedLists');
+            console.log('ERROR getTest');
             this.errorx = error;
         }
     }
 
-    
+ 
    
-    @track statements;
-    @track error;
-    @wire(getStatementList, { parentId: '$recordId' })
-    wiredResult({ error, data }) {
-        console.log('statements');
-        if (data) {
-            console.log('HAS DATA statements');
-            this.statements = data;
-            console.log(this.statements);
-            this.statements.forEach(element => {
-                //console.log(element.Name);
-            });
+    // @track statements;
+    // @track error;
+    // @wire(getStatementList, { parentId: '$recordId' })
+    // wiredResult({ error, data }) {
+    //     console.log('statements');
+    //     if (data) {
+    //         console.log('HAS DATA statements');
+    //         this.statements = data;
+    //         console.log(this.statements);
+    //         this.statements.forEach(element => {
+    //             //console.log(element.Name);
+    //         });
 
 
-        } else if (error) {
-            console.log('ERROR statements');
-            this.error = error;
-        }
-    }
+    //     } else if (error) {
+    //         console.log('ERROR statements');
+    //         this.error = error;
+    //     }
+    // }
 
 
     get currentPlanRelatedLists() {
         return Object.entries(this.recommendations).map((key, value) => ({ key, value }));
     }
+
+
+
+    //handleCheckboxClick() - to update db on checkbox click
+    //actionRecordId : Provide the appropriate Recommended Action object - Id field
+    //isSelected  : Provide the appropriate isSelected value - 0(unselected) / 1(selected)
+
+    handleCheckboxClick() {
+        setActionIsSelected({ ActionRecordId: 'a35Aw00000009phIAA', isSelected: 0 })
+            .then(() => {
+                // Update successful
+                console.log('isSelected updated to successfully!!');
+            })
+            .catch(error => {
+                // Error occurred
+                console.error(error);
+                console.log('isSelected update failed!');
+            });
+    }
+
+
+    //getOtherActionList() - to get other actions list based on main_category and sub_category
+    //category : Provide the appropriate main_category value
+    //indicator : Provide the appropriate sub_category value
+
+    category = 'Employment';
+    indicator = 'Skills';
+
+    @track otherActions;
+    @track error;
+    @wire(getOtherActionList, { category: '$category', indicator:'$indicator'})
+    wiredResult2({ error, data }) {
+        console.log('wiredResult1');
+        if (data) {
+            console.log('HAS DATA Other Actions');
+         
+            this.otherActions = data;
+            console.log(this.otherActions);
+
+        } else if (error) {
+            console.log('ERROR Other Actions');
+            this.errorx = error;
+        }
+    }
+
+
+    //handleOtherActionSelection() - to update db on other action selection
+    //ActionRecordId : Provide the appropriate Recommended Action object - Id field
+    //otherAction : Provide the appropriate other action value (from dropdown)
+
+
+    handleOtherActionSelection() {
+        setOtherAction({ ActionRecordId: 'a35Aw00000009phIAA', otherAction: 'Test saving other action' })
+            .then(() => {
+                // Update successful
+                console.log('otherAction updated to successfully!!');
+            }
+            )   
+            .catch(error => {
+                // Error occurred
+                console.error(error);
+                console.log('otherAction update failed!');
+            }
+            );
+        }   
+        
+        
+    
+    //summaryRecommendations - to get summary recommendations list (only isSelected = 1 actions will be displayed)
+
+    @track summaryRecommendations;
+    @track error;
+    @wire(getPlanRelatedSummaryLists, { planId: '$recordId' })
+    wiredResult3({ error, data }) {
+        console.log('wiredResult3');
+        if (data) {
+            console.log('HAS DATA getPlanRelatedSummaryLists');
+            
+            this.summaryRecommendations = data;
+            console.log(this.summaryRecommendations);
+
+        } else if (error) {
+            console.log('ERROR getPlanRelatedSummaryLists');
+            this.errorx = error;
+        }
+    }
+        
+
 }
