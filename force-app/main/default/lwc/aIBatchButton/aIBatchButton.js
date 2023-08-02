050b5ddf6099dd8c14008ed82d667fa549dafeb0
NarmadaH <achini.nh@gmail.com>
2023-08-02 14:46:53 -0400

    Batch trigger implementation

--- force-app/main/default/lwc/aIBatchButton/aIBatchButton.js
+++ force-app/main/default/lwc/aIBatchButton/aIBatchButton.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
 
-export default class AIBatchButton extends LightningElement {}
+import handleBatchAssessmentResponses from '@salesforce/apex/LWCController.handleBatchAssessmentResponses';
+
+
+export default class AIBatchButton extends LightningElement {
+
+    @api recordId;
+
+    handleButtonClick() {
+        
+        handleBatchAssessmentResponses({ assessmentRecordId:'$recordId' })
+            .then(result => {
+                console.log('Success calling handleBatchAssessmentResponses');
+            })
+            .catch(error => {
+                console.log('Error calling handleBatchAssessmentResponses');
+            });
+    }
+
+}
